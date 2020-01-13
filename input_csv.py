9a245d500f439c8afb18f0c9d7fd190f9895464d
RYOSUKE <ryosuke@h28is009.naist.jp>
2020-01-13 21:55:40 +0900

    add

--- input_csv.py
+++ input_csv.py
@@ -1,7 +1,17 @@
 import csv
+from sklearn.metrics import label_ranking_average_precision_score, average_precision_score
+
 
 with open('pri-similarity.csv','r',encoding = "utf-8-sig") as f:
     # csvをとにかく頭から1行ずつ読み込む
     reader = csv.reader(f)
+    
     for r in reader:
         print(r)
+        ap_array = []
+        for item in r:
+            ap_array.append(int(item))
+        ap = average_precision_score(ap_array, [1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1], average='samples')
+        print(ap_array)
+        print(ap)
+        # print(r)
